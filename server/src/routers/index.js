3ca54fb3b64f8081654334d36a780ce41f83b76c
MinhTri <ngminhtri2203@gmail.com>
2024-12-07 21:33:07 +0700

    new update

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
 import projectRouter from "./projectRouter.js";
+import userRouter from "./userRouter.js"; // Sửa lại đường dẫn
 
 const router = Router();
 
 router.use("/api", authRouter);
 router.use("/api", projectRouter);
+router.use("/api", userRouter); // Sửa lại đường dẫn
 
 export default router;
