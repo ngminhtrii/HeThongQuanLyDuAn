5dd0f5076d12224fdacc61a21a2622e256b90f1d
MinhTri1235 <ngminhtri2203@gmail.com>
2025-06-10 09:55:56 +0700

    test user

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -5,6 +5,7 @@ import userRouter from "./userRouter.js";
 
 const router = Router();
 
+// Registering the routers
 router.use("/api", authRouter);
 router.use("/api", projectRouter);
 router.use("/api", userRouter);
